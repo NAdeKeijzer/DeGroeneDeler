1888b0582af54af4211ddb4221f1738824c29fac
Nikita de Keijzer <n.dk@live.nl>
2025-08-16 14:57:55 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/degroenedeler/DeGroeneDelerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.degroenedeler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DeGroeneDelerApplication
+
+fun main(args: Array<String>) {
+    runApplication<DeGroeneDelerApplication>(*args)
+}
